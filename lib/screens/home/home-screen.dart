b05b56a6b55c94f48915707f0eb1e3f69086a839
shrued <shrutisara10@gmail.com>
2021-01-04 20:35:18 +0530

    homescreen searchbox

--- lib/screens/home/home-screen.dart
+++ lib/screens/home/home-screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/screens/home/components/app_bar.dart';
+import 'package:food_app/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: homeAppBar(context),
+      body: Body(),
     );
   }
 }
