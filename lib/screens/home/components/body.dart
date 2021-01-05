ca1f90e6550eda10a924041122d03328bab5b6f9
shrued <shrutisara10@gmail.com>
2021-01-05 16:19:47 +0530

    homescreen categoryitem

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/components/search_box.dart';
+import 'package:food_app/screens/home/components/category_list.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -9,6 +10,7 @@ class Body extends StatelessWidget {
         SearchBox(
           onChanged: (value) {},
         ),
+        CategoryList(),
       ],
     );
   }
